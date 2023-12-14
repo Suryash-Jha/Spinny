bf2599dd98e768067be50e38d155eab7b76e2955
Suryash <joblesscoder2468@gmail.com>
2023-12-14 19:39:03 +0530

    basic urtls.py and model for handling for box created

--- BoxStoreApp/models.py
+++ BoxStoreApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class BoxModel(models.Model):
+    id= models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    length = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+    area= models.IntegerField()
+    volume= models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.CharField(max_length=100)
+    updated_by = models.CharField(max_length=100)
+    
+    class Meta:
+        db_table = "BoxModel"
+
--- MainProj/settings.py
+++ MainProj/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'BoxStoreApp'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# constraints
+A1= 100
+V1= 1000
+L1= 100
+L2= 50
