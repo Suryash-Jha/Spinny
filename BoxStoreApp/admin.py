baf4bd7597a32ed6196b8daa6c63472aba6592e3
Suryash <joblesscoder2468@gmail.com>
2023-12-17 11:15:20 +0530

    authorization issue solved along with correction in listMe

--- BoxStoreApp/admin.py
+++ BoxStoreApp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import BoxModel, customUser
+from rest_framework.authtoken.admin import TokenAdmin
+
+# import token also
+
+
+admin.site.register(BoxModel)
+admin.site.register(customUser)
+
+
+
+
--- BoxStoreApp/views.py
+++ BoxStoreApp/views.py
@@ -25,8 +25,14 @@ def is_authenticated(req):
     else:
         return False
 
-def is_staff(req):
-    if req.user.is_staff:
+def get_user(req):
+    auth_header = req.headers.get('Authorization')
+    auth_token = auth_header[6:]
+    user = Token.objects.get(key=auth_token).user
+    return user
+
+def is_staff(user):
+    if user.is_staff:
         return True
     else:
         return False
@@ -47,6 +53,7 @@ def register(req):
         return HttpResponse('User created')
     else:
         return HttpResponse('Error creating user')
+
 @csrf_exempt
 def loginx(req):
     if req.method == 'POST':
@@ -80,13 +87,14 @@ def isLogged(req):
 @permission_classes([IsAuthenticated])
 @csrf_exempt
 def addBox(req):
-    if req.method == 'POST' and is_staff(req):
+    user=  get_user(req)
+    if req.method == 'POST' and is_staff(user):
         boxName = req.POST['name']
         boxLength = int(req.POST['length'])
         boxWidth = int(req.POST['width'])
         boxHeight = int(req.POST['height'])
-        boxCreated_by = req.user.username
-        boxUpdated_by = req.user.username
+        boxCreated_by = user.username
+        boxUpdated_by = user.username
         boxArea= 2*(boxLength*boxWidth +boxLength*boxHeight +boxWidth*boxHeight )
         boxVolume= boxLength*boxWidth*boxHeight
         valid, msg= validate_box(boxCreated_by, boxArea, boxVolume)
@@ -107,12 +115,13 @@ def addBox(req):
 @permission_classes([IsAuthenticated])
 @csrf_exempt
 def updateBox(req, id):
-    if req.method == 'POST' and is_staff(req):
+    user=  get_user(req)
+    if req.method == 'POST' and is_staff(user):
         boxName = req.POST['name']
         boxLength = int(req.POST['length'])
         boxWidth = int(req.POST['width'])
         boxHeight = int(req.POST['height'])
-        boxUpdated_by = req.user.username
+        boxUpdated_by = user.username
         boxArea= 2*(boxLength*boxWidth +boxLength*boxHeight +boxWidth*boxHeight )
         boxVolume= boxLength*boxWidth*boxHeight
 
@@ -140,83 +149,102 @@ def updateBox(req, id):
 @permission_classes([IsAuthenticated])
 @csrf_exempt
 def deleteBox(req, id):
+    user=  get_user(req)
     # if req.method == 'DELETE' and is_authenticated(req) and is_staff(req):
-    if req.method == 'DELETE' and is_staff(req):
+    if req.method == 'DELETE' and is_staff(user):
         box = BoxModel.objects.get(id=id)
         box.delete()
         return HttpResponse('Box deleted')
     else:
         return HttpResponse('Error deleting box')
 
+
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 @csrf_exempt
 def listBox(req):
-    if req.method == 'GET' and is_authenticated(req):
-        boxes = BoxModel.objects.all().values()
-        if not is_staff(req):
-            exclude_keys = ['created_by', 'updated_by']
-            # Remove keys from each dictionary in the queryset
-            filtered_boxes = [{key: value for key, value in box.items() if key not in keys_to_exclude} for box in boxes]
-            boxes= filtered_boxes
-            
+    user=  get_user(req)
+    if req.method == 'GET':
         total_area= BoxModel.objects.aggregate(Sum('area'))
         total_volume= BoxModel.objects.aggregate(Sum('volume'))
-        total_individual_box_count= BoxModel.objects.filter(created_by=req.user.username).count()
+        total_individual_box_count= BoxModel.objects.filter(created_by=user.username).count()
         total_boxes= BoxModel.objects.all().count()
-        filtered_data= filter_boxes(req)
-        return JsonResponse({'status': 200,'filtered Data': str(filtered_data), 'user': str(req.user), 'total_area': total_area, 'total_volume': total_volume, 'total_individual_area': total_individual_box_count, 'total_boxes': total_boxes})
+        filtered_data= filter_boxes(req, user)
+        print(filtered_data)
+        return JsonResponse(
+            {
+            'status': 200,
+            'filtered Data': str(filtered_data),
+            'total_boxes_retrieved': len(filtered_data),
+            'user': str(user.username), 
+            'total_area': total_area, 
+            'total_volume': total_volume, 
+            'total_individual_area': total_individual_box_count, 
+            'total_boxes': total_boxes
+            }
+            )
     else:
         return HttpResponse('Error listing boxes')
 
 
 
 
-def filter_boxes(request):
-    if request.method== 'GET':
-        length_more_than = request.GET.get('length_more_than', 0)
-        area_more_than = request.GET.get('area_more_than', 0)
-        volume_more_than = request.GET.get('volume_more_than', 0)
-        height_more_than = request.GET.get('height_more_than', 0)
-        width_more_than = request.GET.get('width_more_than', 0)
+def filter_boxes(request, user):
+    length_more_than = request.GET.get('length_more_than', 0)
+    area_more_than = request.GET.get('area_more_than', 0)
+    volume_more_than = request.GET.get('volume_more_than', 0)
+    height_more_than = request.GET.get('height_more_than', 0)
+    width_more_than = request.GET.get('width_more_than', 0)
 
-        # just for a maximum value as float('inf') is not working
-        maxm= 10000000000
-        length_less_than = request.GET.get('length_less_than', maxm)
-        area_less_than = request.GET.get('area_less_than', maxm)
-        volume_less_than = request.GET.get('volume_less_than', maxm)
-        height_less_than = request.GET.get('height_less_than', maxm)
-        width_less_than = request.GET.get('width_less_than', maxm)
+    # just for a maximum value as float('inf') is not working
+    maxm= 10000000000
+    length_less_than = request.GET.get('length_less_than', maxm)
+    area_less_than = request.GET.get('area_less_than', maxm)
+    volume_less_than = request.GET.get('volume_less_than', maxm)
+    height_less_than = request.GET.get('height_less_than', maxm)
+    width_less_than = request.GET.get('width_less_than', maxm)
 
-        created_by = request.GET.get('created_by', None)
+    created_by = request.GET.get('created_by', None)
 
-        # print('ll:', length_more_than, length_less_than)
-        boxes= BoxModel.objects.filter(
-            Q(length__gt=length_more_than) & 
-            Q(area__gt=area_more_than) & 
-            Q(volume__gt=volume_more_than) & 
-            Q(height__gt=height_more_than) & 
-            Q(width__gt=width_more_than)&
-            Q(length__lt= length_less_than)&
-            Q(width__lt= width_less_than)&
-            Q(height__lt= height_less_than)&
-            Q(area__lt= area_less_than)&
-            Q(volume__lt= volume_less_than)
-            )
-        
-        if created_by:
-            boxes= boxes.filter(created_by= created_by)
-        if not is_staff(req):
-            exclude_keys = ['created_by', 'updated_by']
-            filtered_boxes = [{key: value for key, value in box.items() if key not in keys_to_exclude} for box in boxes]
-            boxes= filtered_boxes
-        # print(boxes.values())
-        return boxes.values()
+    # print('ll:', length_more_than, length_less_than)
+    boxes= BoxModel.objects.filter(
+        Q(length__gte=length_more_than) & 
+        Q(area__gte=area_more_than) & 
+        Q(volume__gte=volume_more_than) & 
+        Q(height__gte=height_more_than) & 
+        Q(width__gte=width_more_than)&
+        Q(length__lte= length_less_than)&
+        Q(width__lte= width_less_than)&
+        Q(height__lte= height_less_than)&
+        Q(area__lte= area_less_than)&
+        Q(volume__lte= volume_less_than)
+        )
+    
+    if created_by:
+        boxes = boxes.filter(created_by=created_by)
+
+    if not is_staff(user):
+        exclude_keys = ['created_by', 'updated_by']
+        filtered_boxes = [{key: value for key, value in box.items() if key not in exclude_keys} for box in boxes.values()]
+        boxes = filtered_boxes
+        return boxes
+    
+    else:
+        filtered_boxes = [{key: value for key, value in box.items()} for box in boxes.values()]
+        boxes = filtered_boxes
+        return boxes
+
+    return boxes.values()
 
 
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 @csrf_exempt
 def listBoxMe(req):
-    if req.method == 'GET' and is_authenticated(req) and is_staff(req):
-        boxes = BoxModel.objects.filter(created_by=req.user.username).values()
-        total_individual_box_count= BoxModel.objects.filter(created_by=req.user.username).count()
-        return JsonResponse({'status': 200,'boxes': str(boxes), 'user': str(req.user),'total_individual_count': total_individual_box_count})
+    user= get_user(req)
+    if req.method == 'GET':
+        boxes = BoxModel.objects.filter(created_by=user.username).values()
+        total_individual_box_count= BoxModel.objects.filter(created_by=user.username).count()
+        return JsonResponse({'status': 200,'boxes': str(boxes), 'user': str(user),'total_individual_count': total_individual_box_count})
     else:
-        return HttpResponse('Error listing boxes')
+        return HttpResponse('Error listing boxes'+ str(user)+ str(user.username))
